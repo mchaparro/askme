2e0c761809a02fa84e93aa5fee8f2d73f50c8e48
Miguel <mx.mik3@gmail.com>
2018-12-12 01:01:00 +0200

    test

--- askme/users/models.py
+++ askme/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
@@ -12,3 +13,21 @@ class User(AbstractUser):
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
+
+
+class Question(models.Model):
+    question = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey('User',blank=True,null=True, related_name='questions', on_delete=models.CASCADE)
+    latitude = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    longitud = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    pending = models.BooleanField (default=True)  
+    
+class Answer(models.Model):
+    question = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey('User',blank=True,null=True, related_name='answers', on_delete=models.CASCADE)    
+
+        
